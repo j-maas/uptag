dd3fc1e745b8177b9e4c92fe8b93e3e8284f062a
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-12-09 11:32:21 +0100

    Use proptest

--- src/version_format.rs
+++ src/version_format.rs
@@ -1,36 +1,36 @@
 use regex::Regex;
 
 /// A version format detecting and comparing versions.
-    ///
-    /// The syntax is based on [Regex], but adds two special escaped characters:
-    /// - `\m` which stands for a semantic version (se**m**antic).
-    /// - `\q` which stands for a sequence number (se**q**uence).
-    ///
-    /// # Examples
-    ///
-    /// Detect only proper SemVer, without any prefix or suffix:
-    ///
-    /// ```rust
-    /// # extern crate updock; use updock::VersionFormat;
-    /// # fn main() {
-    /// let format = VersionFormat::new(r"^\m$").unwrap();
-    /// assert!(format.matches("1.2.3"));
-    /// assert!(!format.matches("1.2.3-debian"));
-    /// # }
-    /// ```
-    ///
-    /// Detect a sequential version after a prefix:
 ///
-    /// ```rust
-    /// # extern crate updock; use updock::VersionFormat;
-    /// # fn main() {
-    /// let format = VersionFormat::new(r"^debian-r\q$").unwrap();
-    /// assert!(format.matches("debian-r24"));
-    /// assert!(!format.matches("debian-r24-alpha"));
-    /// # }
-    /// ```
-    ///
-    /// [Regex]: https://docs.rs/regex/1.3.1/regex/index.html#syntax
+/// The syntax is based on [Regex], but adds two special escaped characters:
+/// - `\m` which stands for a semantic version (se**m**antic).
+/// - `\q` which stands for a sequence number (se**q**uence).
+///
+/// # Examples
+///
+/// Detect only proper SemVer, without any prefix or suffix:
+///
+/// ```rust
+/// # extern crate updock; use updock::VersionFormat;
+/// # fn main() {
+/// let format = VersionFormat::new(r"^\m$").unwrap();
+/// assert!(format.matches("1.2.3"));
+/// assert!(!format.matches("1.2.3-debian"));
+/// # }
+/// ```
+///
+/// Detect a sequential version after a prefix:
+///
+/// ```rust
+/// # extern crate updock; use updock::VersionFormat;
+/// # fn main() {
+/// let format = VersionFormat::new(r"^debian-r\q$").unwrap();
+/// assert!(format.matches("debian-r24"));
+/// assert!(!format.matches("debian-r24-alpha"));
+/// # }
+/// ```
+///
+/// [Regex]: https://docs.rs/regex/1.3.1/regex/index.html#syntax
 #[derive(Debug)]
 pub struct VersionFormat {
     regex: Regex,
@@ -39,10 +39,10 @@ pub struct VersionFormat {
 impl VersionFormat {
     pub fn new(format: &str) -> Result<VersionFormat, regex::Error> {
         let semver_code = r"\m";
-        let normalized_semver = r"(?P<semver>\d+\.\d+\.\d+)";
+        let normalized_semver = r"(?P<semver>[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)";
 
         let sequence_code = r"\q";
-        let normalized_sequence = r"(?P<seq>\d+)";
+        let normalized_sequence = r"(?P<seq>[[:digit:]]+)";
 
         let normalized_format = format
             .replace(semver_code, normalized_semver)
@@ -53,8 +53,8 @@ impl VersionFormat {
         Ok(VersionFormat { regex })
     }
 
-    pub fn matches(&self, version: &str) -> bool {
-        self.regex.is_match(version)
+    pub fn matches<S: AsRef<str>>(&self, version: S) -> bool {
+        self.regex.is_match(version.as_ref())
     }
 }
 
@@ -62,81 +62,65 @@ impl VersionFormat {
 mod tests {
     use super::*;
 
-    macro_rules! assert_matches {
-        ($format:ident, $string:ident) => {
-            assert!(
+    use proptest::prelude::*;
+
+    macro_rules! prop_assert_matches {
+        ($format:ident, $string:expr) => {
+            prop_assert!(
                 $format.matches($string),
-                format!("{:?} did not match '{}'.", $format, $string)
+                "{:?} did not match '{:?}'.",
+                $format,
+                $string
             );
         };
     }
 
-    macro_rules! assert_no_match {
-        ($format:ident, $string:ident) => {
-            assert!(
+    macro_rules! prop_assert_no_match {
+        ($format:ident, $string:expr) => {
+            prop_assert!(
                 !$format.matches($string),
-                format!("{:?} should not match '{}'.", $format, $string)
+                "{:?} should not match '{}'.",
+                $format,
+                $string
             );
         };
     }
 
-    #[test]
-    fn detects_simple_semver() {
-        let format = VersionFormat::new(r"^\m$").unwrap();
-
-        let matches = ["0.1.0", "1.2.3"];
-        for m in &matches {
-            assert_matches!(format, m);
+    proptest! {
+        #[test]
+        fn detects_simple_semver(valid in r"[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+") {
+            let format = VersionFormat::new(r"^\m$").unwrap();
+            prop_assert_matches!(format, &valid);
         }
 
-        let no_matches = ["1", "1.1", "1.1.1-alpha", "debian-1.0.1", "1.0.1-debian"];
-        for m in &no_matches {
-            assert_no_match!(format, m);
-        }
-    }
-
-    #[test]
-    fn detects_surrounded_semver() {
-        let format = VersionFormat::new(r"^v\m-debian$").unwrap();
-
-        let matches = ["v0.1.0-debian", "v1.2.3-debian"];
-        for m in &matches {
-            assert_matches!(format, m);
+        #[test]
+        fn rejects_simple_semver_with_prefix(invalid in r"\PC+[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\PC*") {
+            let format = VersionFormat::new(r"^\m$").unwrap();
+            prop_assert_no_match!(format, &invalid);
         }
 
-        let no_matches = ["1.2.3-debian", "v1.2.3", "1.2.3"];
-        for m in &no_matches {
-            assert_no_match!(format, m);
-        }
-    }
-
-    #[test]
-    fn detects_simple_sequence() {
-        let format = VersionFormat::new(r"^\q$").unwrap();
-
-        let matches = ["1", "100"];
-        for m in &matches {
-            assert_matches!(format, m);
+        #[test]
+        fn rejects_simple_semver_with_suffix(invalid in r"\PC*[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\PC+") {
+            let format = VersionFormat::new(r"^\m$").unwrap();
+            prop_assert_no_match!(format, &invalid);
         }
 
-        let no_matches = ["1.", "1.0", "debian-3", "123-debian"];
-        for m in &no_matches {
-            assert_no_match!(format, m);
-        }
-    }
-
-    #[test]
-    fn detects_surrounded_sequence() {
-        let format = VersionFormat::new(r"^release:\q-debian$").unwrap();
-
-        let matches = ["release:0-debian", "release:1393-debian"];
-        for m in &matches {
-            assert_matches!(format, m);
+        #[test]
+        fn detects_simple_sequence(valid in r"[[:digit:]]+") {
+            let format = VersionFormat::new(r"^\q$").unwrap();
+            prop_assert_matches!(format, &valid);
         }
 
-        let no_matches = ["10-debian", "release:4", "123"];
-        for m in &no_matches {
-            assert_no_match!(format, m);
+        #[test]
+        fn rejects_simple_sequence_with_prefix(invalid in r"\PC+[[:digit:]]+\PC*") {
+            let format = VersionFormat::new(r"^\q$").unwrap();
+            prop_assert_no_match!(format, &invalid);
+        }
+
+        #[test]
+        fn rejects_simple_sequence_with_suffix(invalid in r"\PC*[[:digit:]]+\PC+") {
+            let format = VersionFormat::new(r"^\q$").unwrap();
+            prop_assert_no_match!(format, &invalid);
         }
     }
 }
